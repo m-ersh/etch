56b62e23035fb430c7f5041a215698b0b0afb08e
m-ersh <206779368+m-ersh@users.noreply.github.com>
2025-05-06 12:13:37 -0400

    Updated to default to random background colors.

--- javascript.js
+++ javascript.js
@@ -12,21 +12,24 @@
 
 const squares = 256;
 
-
-
-
 const container = document.createElement("div");
 container.classList.add("container")
 document.body.appendChild(container);
 
+const background = (divs) => { 
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    divs.style.backgroundColor = "#" + randomColor;
+};
 
 function addSquares() {
     for (let i = 0; i < squares; i++) {
         const divs = document.createElement("div");
         divs.classList.add("divs");
         container.appendChild(divs);
-        divs.addEventListener("mouseover", function() {
-            divs.style.backgroundColor = "red";
+        divs.addEventListener("mouseover", () => {
+            background(divs);
         });
     }
 };
+
+addSquares();
